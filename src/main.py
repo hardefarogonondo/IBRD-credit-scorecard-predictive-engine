7f8e1cfeb445570604e292e542a4c3fac45529f4
hardefarogonondo <hardefarogonondo@gmail.com>
2023-07-02 01:52:34 +0700

    Initial data transformation functions updated

--- src/main.py
+++ src/main.py
@@ -1,38 +1,78 @@
+# Import Libraries
 from fastapi import FastAPI
 from pydantic import BaseModel
+import numpy as np
 import pandas as pd
 import pickle
-import numpy as np
 
-# load the scorecard
+# Initialization
 with open('../../models/final_scorecard.pkl', 'rb') as file:
     scorecard = pickle.load(file)
-
 app = FastAPI()
-
-# Define a Pydantic model to handle incoming request body
 class Item(BaseModel):
     data: dict
 
 @app.post('/score')
 def score_data(item: Item):
-    # convert the incoming json to a pandas dataframe
-    raw_data = pd.DataFrame(item.data, index=[0])
-
-    # transform the raw data to match the scorecard
+    raw_data = pd.DataFrame(item.data, index = [0])
     transformed_data = transform_data(raw_data)
-
-    # dot product with scorecard
     score = calculate_score(transformed_data)
+    return {"score": score}
 
-    return {'score': score}
+def region_binning(df):
+    df["region"] = df["region"].replace(regions_bin)
+    return df
+
+def country_binning(df, groups):
+    for i, group in enumerate(groups):
+        for country in group:
+            df.loc[df["country"] == country, "country"] = f'group_{chr(97+i)}'
+    remaining_countries = set(df["country"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
+    last_group_name = f'group_{chr(97+len(groups)-1)}'
+    for country in remaining_countries:
+        df.loc[df["country"] == country, "country"] = last_group_name
+    return df
+
+def guarantor_binning(df, groups):
+    for i, group in enumerate(groups):
+        for guarantor in group:
+            df.loc[df["guarantor"] == guarantor, "guarantor"] = f'group_{chr(97+i)}'
+    remaining_guarantors = set(df["guarantor"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
+    last_group_name = f'group_{chr(97+len(groups)-1)}'
+    for guarantor in remaining_guarantors:
+        df.loc[df["guarantor"] == guarantor, "guarantor"] = last_group_name
+    df["guarantor"] = df["guarantor"].fillna(last_group_name)
+    return df
+
+def loan_type_binning(df, groups):
+    for i, group in enumerate(groups):
+        for loan_type in group:
+            df.loc[df["loan_type"] == loan_type, "loan_type"] = f'group_{chr(97+i)}'
+    remaining_loan_types = set(df["loan_type"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
+    return df
+
+def principal_amount_binning(df):
+    bins = [-np.inf, 1978840.31, 8013587.75, 23240000.00, 40215236.00, 54947816.00, 249441952.00, np.inf]
+    bin_labels = ["-inf_to_1978840.31", "1978840.31_to_8013587.75", "8013587.75_to_23240000.00", "23240000.00_to_40215236.00", "40215236.00_to_54947816.00", "54947816.00_to_249441952.00", "249441952.00_to_inf"]
+    df["principal_amount"] = pd.cut(df["principal_amount"], bins = bins, labels = bin_labels)
+    df["principal_amount"] = df["principal_amount"].astype(str)
+    return df
+
+def feature_binning(df):
+    df = region_binning(df)
+    df = country_binning(df, country_groups)
+    df = guarantor_binning(df, guarantor_groups)
+    df = loan_type_binning(df, loan_type_groups)
+    df = principal_amount_binning(df)
+    return df
 
 def transform_data(raw_data):
-    # Include all necessary transformations on your incoming data
-    # to match the form of the data used to create the scorecard.
-
-    # Example: one-hot encoding a categorical feature
-    raw_data = pd.get_dummies(raw_data, columns=['Region', 'Country', 'Guarantor', 'Loan Type'])
+    binned_data = feature_binning(raw_data)
+    raw_data = pd.get_dummies(df,
+                                columns = feature_to_encode,
+                                prefix = feature_to_encode,
+                                prefix_sep = ":")
+    encoded_data = raw_data.astype(int)
 
     # If your scorecard expects certain columns that might be missing in the input data,
     # you should add those columns with default values. Example:
@@ -44,7 +84,7 @@ def transform_data(raw_data):
     # Make sure the column order matches the scorecard
     raw_data = raw_data[expected_columns]
 
-    return raw_data
+    return encoded_data
 
 
 def calculate_score(transformed_data):
