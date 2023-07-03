5b3db2b41429268e1ea52920c6f6bc2a53b61e75
hardefarogonondo <hardefarogonondo@gmail.com>
2023-07-04 03:02:08 +0700

    Finished scoring updated

--- src/back_end/main.py
+++ src/back_end/main.py
@@ -1,6 +1,7 @@
 # Import Libraries
 from fastapi import FastAPI
 from pydantic import BaseModel
+import json
 import numpy as np
 import pandas as pd
 import pickle
@@ -8,85 +9,84 @@ import pickle
 # Initialization
 with open('../../models/final_scorecard.pkl', 'rb') as file:
     scorecard = pickle.load(file)
+with open('../config/binnings.json', 'r') as file:
+    binnings = json.load(file)
 app = FastAPI()
 class Item(BaseModel):
     data: dict
 
+def country_guarantor_status_generator(raw_data):
+    raw_data["country_guarantor_status"] = raw_data.apply(lambda row: "same" if row["country"] == row["guarantor"]
+                                                          else ("no_guarantor"
+                                                                if pd.isnull(row["guarantor"])
+                                                                else "differ"), axis = 1)
+    return raw_data
+
+def region_binning(raw_data):
+    raw_data["region"] = raw_data["region"].replace(binnings["regions_bin"])
+    return raw_data
+
+def country_binning(raw_data):
+    country_bins = binnings["country_bins"]
+    for group, countries in country_bins.items():
+        raw_data.loc[raw_data["country"].isin(countries), "country"] = group
+    return raw_data
+
+def guarantor_binning(raw_data):
+    guarantor_bins = binnings["guarantor_bins"]
+    for group, guarantors in guarantor_bins.items():
+        raw_data.loc[raw_data["guarantor"].isin(guarantors), "guarantor"] = group
+    return raw_data
+
+def loan_type_binning(raw_data):
+    loan_type_bins = binnings["loan_type_bins"]
+    for group, loan_types in loan_type_bins.items():
+        raw_data.loc[raw_data["loan_type"].isin(loan_types), "loan_type"] = group
+    return raw_data
+
+def principal_amount_binning(raw_data):
+    principal_amount_bins = binnings["principal_amount_bins"]
+    raw_data["bins"] = np.nan
+    for bin in principal_amount_bins:
+        lower, upper, bin_name = bin
+        mask = (raw_data["principal_amount"].astype(float) > lower) & (raw_data["principal_amount"].astype(float) <= upper)
+        raw_data.loc[mask, "bins"] = bin_name
+    raw_data["principal_amount"] = raw_data["bins"]
+    raw_data = raw_data.drop(columns = "bins")
+    return raw_data
+
+def feature_engineering(raw_data):
+    binned_data = country_guarantor_status_generator(raw_data)
+    binned_data = region_binning(binned_data)
+    binned_data = country_binning(binned_data)
+    binned_data = guarantor_binning(binned_data)
+    binned_data = loan_type_binning(binned_data)
+    binned_data = principal_amount_binning(binned_data)
+    return binned_data
+
+def one_hot_encoding(binned_data):
+    category_dict = binnings["categories"]
+    for feature, categories in category_dict.items():
+        for category in categories:
+            binned_data[f"{feature}:{category}"] = (binned_data[feature] == category).astype(int)
+        binned_data = binned_data.drop(columns = feature)
+    return binned_data
+
+def get_score(encoded_data, scorecard):
+    encoded_data_copy = encoded_data.copy()
+    missing_cols = set(scorecard["Feature Name"]) - set(encoded_data.columns)
+    for column in missing_cols:
+        encoded_data_copy[column] = 0
+    encoded_data_copy["Intercept"] = 1
+    encoded_data_copy = encoded_data_copy[scorecard["Feature Name"]]
+    scorecard_scores = scorecard["Score - Final"].values.reshape(scorecard.shape[0], 1)
+    scored_data = encoded_data_copy.dot(scorecard_scores)
+    return int(scored_data.values[0][0])
+
 @app.post('/score')
 def score_data(item: Item):
     raw_data = pd.DataFrame(item.data, index = [0])
-    transformed_data = transform_data(raw_data)
-    score = calculate_score(transformed_data)
+    binned_data = feature_engineering(raw_data)
+    encoded_data = one_hot_encoding(binned_data)
+    score = get_score(encoded_data, scorecard)
     return {"score": score}
-
-def region_binning(df):
-    df["region"] = df["region"].replace(regions_bin)
-    return df
-
-def country_binning(df, groups):
-    for i, group in enumerate(groups):
-        for country in group:
-            df.loc[df["country"] == country, "country"] = f'group_{chr(97+i)}'
-    remaining_countries = set(df["country"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
-    last_group_name = f'group_{chr(97+len(groups)-1)}'
-    for country in remaining_countries:
-        df.loc[df["country"] == country, "country"] = last_group_name
-    return df
-
-def guarantor_binning(df, groups):
-    for i, group in enumerate(groups):
-        for guarantor in group:
-            df.loc[df["guarantor"] == guarantor, "guarantor"] = f'group_{chr(97+i)}'
-    remaining_guarantors = set(df["guarantor"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
-    last_group_name = f'group_{chr(97+len(groups)-1)}'
-    for guarantor in remaining_guarantors:
-        df.loc[df["guarantor"] == guarantor, "guarantor"] = last_group_name
-    df["guarantor"] = df["guarantor"].fillna(last_group_name)
-    return df
-
-def loan_type_binning(df, groups):
-    for i, group in enumerate(groups):
-        for loan_type in group:
-            df.loc[df["loan_type"] == loan_type, "loan_type"] = f'group_{chr(97+i)}'
-    remaining_loan_types = set(df["loan_type"]) - set([f'group_{chr(97+i)}' for i in range(len(groups))])
-    return df
-
-def principal_amount_binning(df):
-    bins = [-np.inf, 1978840.31, 8013587.75, 23240000.00, 40215236.00, 54947816.00, 249441952.00, np.inf]
-    bin_labels = ["-inf_to_1978840.31", "1978840.31_to_8013587.75", "8013587.75_to_23240000.00", "23240000.00_to_40215236.00", "40215236.00_to_54947816.00", "54947816.00_to_249441952.00", "249441952.00_to_inf"]
-    df["principal_amount"] = pd.cut(df["principal_amount"], bins = bins, labels = bin_labels)
-    df["principal_amount"] = df["principal_amount"].astype(str)
-    return df
-
-def feature_binning(df):
-    df = region_binning(df)
-    df = country_binning(df, country_groups)
-    df = guarantor_binning(df, guarantor_groups)
-    df = loan_type_binning(df, loan_type_groups)
-    df = principal_amount_binning(df)
-    return df
-
-def transform_data(raw_data):
-    binned_data = feature_binning(raw_data)
-    raw_data = pd.get_dummies(df,
-                                columns = feature_to_encode,
-                                prefix = feature_to_encode,
-                                prefix_sep = ":")
-    encoded_data = raw_data.astype(int)
-
-    # If your scorecard expects certain columns that might be missing in the input data,
-    # you should add those columns with default values. Example:
-    expected_columns = list(scorecard['Feature Name'])
-    for column in expected_columns:
-        if column not in raw_data.columns:
-            raw_data[column] = 0
-
-    # Make sure the column order matches the scorecard
-    raw_data = raw_data[expected_columns]
-
-    return encoded_data
-
-
-def calculate_score(transformed_data):
-    score = np.dot(transformed_data.values, scorecard['Score - Final'].values)
-    return score
