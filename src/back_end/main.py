d8cf936eabdea2c702c3a4dd2ccd54f512337db6
hardefarogonondo <hardefarogonondo@gmail.com>
2023-07-10 17:41:15 +0700

    Model and config path modified

--- src/back_end/main.py
+++ src/back_end/main.py
@@ -7,24 +7,29 @@ import pandas as pd
 import pickle
 
 # Initialization
-with open('../../models/final_scorecard.pkl', 'rb') as file:
+with open('/app/models/final_scorecard.pkl', 'rb') as file:
     scorecard = pickle.load(file)
-with open('../config/binnings.json', 'r') as file:
+with open('/app/config/binnings.json', 'r') as file:
     binnings = json.load(file)
 app = FastAPI()
+
+
 class Item(BaseModel):
     data: dict
+
 
 def country_guarantor_status_generator(raw_data):
     raw_data["country_guarantor_status"] = raw_data.apply(lambda row: "same" if row["country"] == row["guarantor"]
                                                           else ("no_guarantor"
                                                                 if pd.isnull(row["guarantor"])
-                                                                else "differ"), axis = 1)
+                                                                else "differ"), axis=1)
     return raw_data
+
 
 def region_binning(raw_data):
     raw_data["region"] = raw_data["region"].replace(binnings["regions_bin"])
     return raw_data
+
 
 def country_binning(raw_data):
     country_bins = binnings["country_bins"]
@@ -32,28 +37,35 @@ def country_binning(raw_data):
         raw_data.loc[raw_data["country"].isin(countries), "country"] = group
     return raw_data
 
+
 def guarantor_binning(raw_data):
     guarantor_bins = binnings["guarantor_bins"]
     for group, guarantors in guarantor_bins.items():
-        raw_data.loc[raw_data["guarantor"].isin(guarantors), "guarantor"] = group
+        raw_data.loc[raw_data["guarantor"].isin(
+            guarantors), "guarantor"] = group
     return raw_data
+
 
 def loan_type_binning(raw_data):
     loan_type_bins = binnings["loan_type_bins"]
     for group, loan_types in loan_type_bins.items():
-        raw_data.loc[raw_data["loan_type"].isin(loan_types), "loan_type"] = group
+        raw_data.loc[raw_data["loan_type"].isin(
+            loan_types), "loan_type"] = group
     return raw_data
+
 
 def principal_amount_binning(raw_data):
     principal_amount_bins = binnings["principal_amount_bins"]
     raw_data["bins"] = np.nan
     for bin in principal_amount_bins:
         lower, upper, bin_name = bin
-        mask = (raw_data["principal_amount"].astype(float) > lower) & (raw_data["principal_amount"].astype(float) <= upper)
+        mask = (raw_data["principal_amount"].astype(float) > lower) & (
+            raw_data["principal_amount"].astype(float) <= upper)
         raw_data.loc[mask, "bins"] = bin_name
     raw_data["principal_amount"] = raw_data["bins"]
-    raw_data = raw_data.drop(columns = "bins")
+    raw_data = raw_data.drop(columns="bins")
     return raw_data
+
 
 def feature_engineering(raw_data):
     binned_data = country_guarantor_status_generator(raw_data)
@@ -64,13 +76,16 @@ def feature_engineering(raw_data):
     binned_data = principal_amount_binning(binned_data)
     return binned_data
 
+
 def one_hot_encoding(binned_data):
     category_dict = binnings["categories"]
     for feature, categories in category_dict.items():
         for category in categories:
-            binned_data[f"{feature}:{category}"] = (binned_data[feature] == category).astype(int)
-        binned_data = binned_data.drop(columns = feature)
+            binned_data[f"{feature}:{category}"] = (
+                binned_data[feature] == category).astype(int)
+        binned_data = binned_data.drop(columns=feature)
     return binned_data
+
 
 def get_score(encoded_data, scorecard):
     encoded_data_copy = encoded_data.copy()
@@ -79,13 +94,15 @@ def get_score(encoded_data, scorecard):
         encoded_data_copy[column] = 0
     encoded_data_copy["Intercept"] = 1
     encoded_data_copy = encoded_data_copy[scorecard["Feature Name"]]
-    scorecard_scores = scorecard["Score - Final"].values.reshape(scorecard.shape[0], 1)
+    scorecard_scores = scorecard["Score - Final"].values.reshape(
+        scorecard.shape[0], 1)
     scored_data = encoded_data_copy.dot(scorecard_scores)
     return int(scored_data.values[0][0])
 
+
 @app.post('/score')
 def score_data(item: Item):
-    raw_data = pd.DataFrame(item.data, index = [0])
+    raw_data = pd.DataFrame(item.data, index=[0])
     binned_data = feature_engineering(raw_data)
     encoded_data = one_hot_encoding(binned_data)
     score = get_score(encoded_data, scorecard)
